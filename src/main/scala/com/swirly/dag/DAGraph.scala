9735efc9e4ae0da88b3a7a19bd93cfd235266458
Iskandar <iskandar@MacBook-Pro-Iskandar.local>
2016-12-06 10:43:39 +0300

    dag cycle check (Kahn algorithm)

--- src/main/scala/com/swirly/dag/DAGraph.scala
+++ src/main/scala/com/swirly/dag/DAGraph.scala
@@ -1,23 +1,69 @@
 package com.swirly.dag
+
 import java.util.UUID
+import scala.collection.mutable.ListBuffer
 
-/**
-  * Created by Bulat on 29.11.2016.
-  */
+class Node(val uid: UUID, val url: String) {
 
-class Node[T](val id: UUID, val data: T) {
-
+  override def toString: String = "Node uid:" + uid + " url:" + url
 }
 
-class Link[T](val source: Node[T], val destination: Node[T]) {
+class Link(val source: String, val destination: String) {
 
+  override def toString: String = "Edge " + source + " -> " + destination
 }
 
-class DAGraph[T](val nodes: Seq[Node[T]], val links: Seq[Link[T]]) {
+class DAGraph(val nodes: Seq[Node], val links: Seq[Link]) {
+  def in (node: Node): Seq[Link] = ???
+  def out(node: Node): Seq[Link] = ???
 
-  def in (node: Node[T]): Seq[Link[T]] = ???
-  def srcs(node: Node[T]): Seq[Node[T]] = ???
+  /**
+    * returns true if graph has no cycles;
+    * it also sorts graph in topological order;
+    *
+    * @return Boolean
+    */
+  def Kahn(): Boolean = {
+    def getRoots(): Seq[Node] = {
+      val destinations = links.map(_.destination)
+      nodes.filter(x => !destinations.contains(x.url))
+    }
 
-  def out(node: Node[T]): Seq[Link[T]] = ???
-  def dests(node: Node[T]): Seq[Node[T]] = ???
+    var S = getRoots().to[ListBuffer]
+    var edges = links.to[ListBuffer]
+    val L = new ListBuffer[Node]()
+
+    while (S.nonEmpty) {
+      val n = S.head
+      S -= n
+      L += n
+
+      for (e <- edges.filter(x => x.source == n.url)) {
+        val m = e.destination
+        edges -= e
+        if (!edges.exists(x => x.destination == m)) S += nodes.filter(x => x.url == m).head
+      }
+    }
+
+    edges.isEmpty
+  }
 }
+
+//// TODO: tests
+//object Test extends App {
+//  val node1 = new Node(UUID.randomUUID(), "url1")
+//  val node2 = new Node(UUID.randomUUID(), "url2")
+//  val node3 = new Node(UUID.randomUUID(), "url3")
+//  val node4 = new Node(UUID.randomUUID(), "url4")
+//  val nodes = Seq(node1, node2, node3, node4)
+//  val edges = Seq(
+//    new Link(node1.url, node2.url),
+//    new Link(node2.url, node4.url),
+//    new Link(node3.url, node4.url),
+//    new Link(node3.url, node2.url),
+//    new Link(node1.url, node3.url)
+//  )
+//  val graph = new DAGraph(nodes, edges)
+//  print(graph.Kahn())
+//}
+
